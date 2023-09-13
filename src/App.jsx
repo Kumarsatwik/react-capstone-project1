f0c30fa3e73502ae91252a3d8c2b4e464795c065
kumarsatwik <38569860+Kumarsatwik@users.noreply.github.com>
2023-09-13 12:17:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Movie from "./Components/Movie/Movie";
 import Index from "./Components/category/Index";
-import Dashboard from "./Components/dashboard/dashboard";
+import Dashboard from "./Components/dashboard/Dashboard";
 import Home from "./Components/home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
